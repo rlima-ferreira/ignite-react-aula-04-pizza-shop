2fbdb0a0c1fdf46d10f1ecbc3824dc9c31ded294
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2024-03-31 08:12:03 -0300

    Implementação do tema da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
@@ -11,7 +12,9 @@ export function App() {
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
       <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
